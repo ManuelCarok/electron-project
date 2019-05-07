c66d56c4e55cde2644f84376c18c0f5a1f165e3a
Manuel Caroca <manuel@izytech.com>
2019-05-07 17:31:31 -0400

    evento click

--- src/renderer/frontend.js
+++ src/renderer/frontend.js
@@ -1,3 +1,23 @@
+import os from 'os';
+
 window.addEventListener('load', () => {
     // document.getElementById('mensaje').innerHTML = "este es un mensaje";
+    console.log(os.cpus());
+    addImagesEvents();
 });
+
+function addImagesEvents() {
+    const thumbs = document.querySelectorAll('li.list-group-item');
+
+    for (let i = 0, lengthT = thumbs.length; i < lengthT; i++) {
+        thumbs[i].addEventListener('click', function() {
+            changeImage(this)
+        });
+    }
+}
+
+function changeImage(nodo) {
+    document.querySelector('li.selected').classList.remove('selected');
+    nodo.classList.add('selected');
+    document.getElementById('image-displayed').src = nodo.querySelector('img').src;
+}
