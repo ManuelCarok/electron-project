5c862a0b551af58a863ee3fc5ef8bace90cbe082
Manuel Caroca <manuelcarok@gmail.com>
2019-05-06 22:46:23 -0300

    eventos y configuraciones

--- README.md
+++ README.md
@@ -7,7 +7,20 @@ Ejecutar en el directorio creado por cmd `npm init`
 crear gitignore con: 
 
 ```
-
+.DS_Store
+Trumbs.db
+node_modules
+npm-debug.log
+debug.log
 ```
 
 Instalar electron: `npm i electron --save` (no se recomienda instalar de forma global)
+
+se crea script en package.json:
+
+```json
+"scripts": {
+    "start": "electron index.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+}
+```
--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 'use strict'
 
+// Instanciando los objectos app y BrowserWindow
 const { app, BrowserWindow } = require('electron');
 
-console.dir(app);
-
+// imprimiendo un mensaje por consola antes de salir
 app.on('before-quit', () => {
     console.log('saliendo...');
 });
 
+// ejecutando ordenes cuando la app esta lista
 app.on('ready', () => {
-    let win = new BrowserWindow()
 
+    // creando una ventana
+    let win = new BrowserWindow({
+        width: 800, // Ancho px
+        height: 600, // Alto px
+        title: 'Electron project', // Titulo de la ventana
+        center: true, // Se muestre en el centro de la pantalla
+        maximizable: false, // No se puede maximizar
+        show: false, // Ventana invisible
+    });
+
+    // .once solo se ejecuta una vez, ready-to-show es el evento que se ejecuta cuando el contenido esta listo
+    win.once('ready-to-show', () => {
+        win.show(); // mostrar ventana
+    });
+
+    // dectectando cuando la ventana se mueve
+    win.on('move', () => {
+        const posicion = win.getPosition(); // Trae la posicion de la ventana
+        console.log(`la posicion es ${ posicion }`);
+    });
+
+    // detectando el cierre de la ventana para cerrar la app
     win.on('closed', () => {
         win = null;
         app.quit();
     });
+
+    win.loadURL('https://devdocs.io/'); // cargar una web en electron
 });
