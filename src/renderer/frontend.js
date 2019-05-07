bad2e79b8bbead2e991eca1103a8ada22126613e
Manuel Caroca <manuel@izytech.com>
2019-05-07 17:52:34 -0400

    filtro

--- src/renderer/frontend.js
+++ src/renderer/frontend.js
@@ -1,9 +1,12 @@
 import os from 'os';
+import url from 'url';
+import path from 'path';
 
 window.addEventListener('load', () => {
     // document.getElementById('mensaje').innerHTML = "este es un mensaje";
     console.log(os.cpus());
     addImagesEvents();
+    searImagesEvent();
 });
 
 function addImagesEvents() {
@@ -20,4 +23,34 @@ function changeImage(nodo) {
     document.querySelector('li.selected').classList.remove('selected');
     nodo.classList.add('selected');
     document.getElementById('image-displayed').src = nodo.querySelector('img').src;
+}
+
+function searImagesEvent() {
+    const searchBox = document.getElementById('search-box');
+
+    searchBox.addEventListener('keyup', function() {
+        const regex = new RegExp(this.value.toLowerCase(), 'gi');
+
+        if (this.value.length > 0) {
+            const thumbs = document.querySelectorAll('li.list-group-item img');
+
+            for (let i = 0, lengthT = thumbs.length; i < lengthT; i++) {
+                const fileUrl = url.parse(thumbs[i].src);
+                const fileName = path.basename(fileUrl.pathname);
+
+                if (fileName.match(regex)) {
+                    thumbs[i].parentNode.classList.remove('hidden');
+                } else {
+                    thumbs[i].parentNode.classList.add('hidden');
+                }
+            }
+
+            selectFirstImage();
+        }
+    });
+}
+
+function selectFirstImage() {
+    const img = document.querySelector('li.list-group-item:not(.hidden)');
+    changeImage(img);
 }
