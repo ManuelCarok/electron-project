3fd18085a4872643d0a3d93e468615c280672edf
Manuel Caroca <manuelcarok@gmail.com>
2019-05-21 23:26:20 -0400

    Cajas de mensajes

--- src/renderer/ipcRendererEvents.js
+++ src/renderer/ipcRendererEvents.js
@@ -12,7 +12,11 @@ function setIpc() {
     });
 
     ipcRenderer.on('save-image', (event, file) => {
-        saveImage(file);
+        saveImage(file, (err) => {
+            if(err) return showDialog('error', 'Electron', err.message)
+            
+            showDialog('info', 'Electron', 'Se guardo')
+        });
     });
 }
 
@@ -26,6 +30,10 @@ function saveFile() {
     const ext = path.extname(image);
     
     ipcRenderer.send('open-save-dialog', ext);
+}
+
+function showDialog(type, title, msg) {
+    ipcRenderer.send('show-dialog', { type: type, title: title, message: msg });
 }
 
 module.exports = {
